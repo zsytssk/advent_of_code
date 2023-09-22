7d019513749dbed77918bbc574b19f451448bd12
张世阳 <zsytssk@gmail.com>
2023-09-22 10:03:14 +0800

    [master]

--- doc/note.md
+++ doc/note.md
@@ -3,3 +3,7 @@ https://adventofcode.com/2022/day/1
 ownerproof-3155442-1695261524-344d6e3a17b9
 
 https://song.xlog.app/aoc-zh
+
+## 2023-09-22 09:42:11
+
+- @ques 如何 split with regex
--- src/day1/mod.rs
+++ src/day1/mod.rs
@@ -1,6 +1,36 @@
 use crate::utils::read_file;
+use regex::Regex;
 
 pub fn parse() {
     let content = read_file("day1/input.txt").unwrap();
-    println!("{content}");
+    let multi_line_reg = Regex::new(r"\n{2,}").unwrap();
+    let line_reg = Regex::new(r"\n").unwrap();
+    // let arr: i32 = reg
+    //     .split(&content)
+    //     .map(|x| x.split("\n").map(|x| x.parse::<i32>().unwrap()).sum())
+    //     .max()
+    //     .unwrap();
+
+    let arr: Vec<Vec<i32>> = multi_line_reg
+        .split(&content)
+        .map(|line| {
+            line_reg
+                .split(line)
+                .map(|num_str| num_str.parse::<i32>().unwrap())
+                .collect()
+        })
+        .collect();
+
+    println!("{:?}", arr);
 }
+// pub fn parse() {
+//     let content = read_file("day1/input.txt").unwrap();
+
+//     let arr: i32 = content
+//         .split("\n\n")
+//         .map(|x| x.split("\n").map(|x| x.parse::<i32>().unwrap()).sum())
+//         .max()
+//         .unwrap();
+
+//     println!("{:?}", arr);
+// }
