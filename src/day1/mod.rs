d787e3f741f76956390fe506301ba2a1c79d38e2
张世阳 <zsytssk@gmail.com>
2023-09-23 13:54:00 +0800

    [master]

--- src/day1/mod.rs
+++ src/day1/mod.rs
@@ -2,35 +2,41 @@ use crate::utils::read_file;
 use regex::Regex;
 
 pub fn parse() {
+    parse1();
+
+    parse2_1();
+}
+fn parse2_1() {
+    let content = read_file("day1/input2.txt").unwrap();
+
+    let mut arr: Vec<i32> = content
+        .split("\n\n")
+        .map(|x| x.split("\n").map(|x| x.parse::<i32>().unwrap()).sum())
+        .collect();
+
+    arr.sort();
+
+    let len = arr.len();
+    let a: i32 = arr[len - 3..len].iter().sum();
+
+    println!("parse1: {:?}", a);
+}
+
+fn parse1() {
     let content = read_file("day1/input.txt").unwrap();
     let multi_line_reg = Regex::new(r"\n{2,}").unwrap();
     let line_reg = Regex::new(r"\n").unwrap();
-    // let arr: i32 = reg
-    //     .split(&content)
-    //     .map(|x| x.split("\n").map(|x| x.parse::<i32>().unwrap()).sum())
-    //     .max()
-    //     .unwrap();
 
-    let arr: Vec<Vec<i32>> = multi_line_reg
+    let arr: i32 = multi_line_reg
         .split(&content)
         .map(|line| {
             line_reg
                 .split(line)
                 .map(|num_str| num_str.parse::<i32>().unwrap())
-                .collect()
+                .sum()
         })
-        .collect();
+        .max()
+        .unwrap();
 
-    println!("{:?}", arr);
+    println!("parse2: {:?}", arr);
 }
-// pub fn parse() {
-//     let content = read_file("day1/input.txt").unwrap();
-
-//     let arr: i32 = content
-//         .split("\n\n")
-//         .map(|x| x.split("\n").map(|x| x.parse::<i32>().unwrap()).sum())
-//         .max()
-//         .unwrap();
-
-//     println!("{:?}", arr);
-// }
