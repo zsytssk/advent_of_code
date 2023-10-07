ac65ac0c9d64773d1fd302425d8aed1396f4e371
张世阳 <zsytssk@gmail.com>
2023-10-07 15:18:29 +0800

    [master]

--- src/day12/mod.rs
+++ src/day12/mod.rs
@@ -1,4 +1,6 @@
 #![allow(unused)]
+use std::cell::{Ref, RefCell, RefMut};
+
 use crate::utils::read_file;
 
 mod point;
@@ -17,34 +19,110 @@ pub fn parse() {
 fn parse1() {
     let map = parse_input();
 
-    let mut start = Point::init();
-    let mut end = Point::init();
+    let mut start = None;
+    let mut end = None;
 
-    for i in 0..map.len() {
-        let line = &map[i];
-        for j in 0..line.len() {
-            if line[j] == "S" {
-                start.update_pos(i, j);
+    for y in 0..map.y {
+        for x in 0..map.x {
+            let item = map.get_point(x, y).unwrap().borrow();
+            if item.has_letter("S") {
+                start = Some(item);
+                continue;
             }
-            if line[j] == "E" {
-                end.update_pos(i, j);
+            if item.has_letter("E") {
+                end = Some(item);
             }
         }
     }
 
+    // println!("{:?}", map);
     println!("{:?} {:?}", start, end);
 }
 
-fn parse_input() -> Vec<Vec<String>> {
+fn parse_input() -> Map {
     let content = read_file("day12/demo.txt").unwrap();
 
-    content
+    let map_str = content
         .split("\n")
         .map(|line| {
             line.split("")
                 .filter(|&item| item != "")
-                .map(String::from)
                 .collect::<Vec<_>>()
         })
-        .collect::<Vec<_>>()
+        .collect::<Vec<_>>();
+
+    let mut map = Map::new(map_str[0].len(), map_str.len());
+    for y in 0..map_str.len() {
+        let line_str = &map_str[y];
+        for x in 0..line_str.len() {
+            let p = Point::init(x, y, String::from(line_str[x]));
+            map.add_point(p);
+        }
+    }
+
+    for y in 0..map_str.len() {
+        let line_str = &map_str[y];
+        for x in 0..line_str.len() {
+            match map.get_point(x, y) {
+                None => continue,
+                Some(p) => check_move(p, &map),
+            }
+        }
+    }
+
+    map
+}
+
+fn check_move(mut point: &RefCell<Point>, map: &Map) {
+    let arr = [Dir::Up, Dir::Down, Dir::Left, Dir::Right];
+    let mut dir_arr = Vec::new();
+    for dir in arr {
+        let diff_pos = get_dir_pos(point.borrow(), &dir, map);
+        match diff_pos {
+            Some(p) => match map.get_point(p.0, p.1) {
+                None => {}
+                Some(diff_point) => {
+                    if (point.borrow().can_move(diff_point.borrow())) {
+                        dir_arr.push(dir.clone());
+                    }
+                }
+            },
+            None => {}
+        }
+    }
+
+    point.borrow_mut().set_move_dir(dir_arr);
+}
+
+fn get_dir_pos(
+    point: Ref<Point>,
+    dir: &Dir,
+    map: &Map,
+) -> Option<(usize, usize)> {
+    match dir {
+        Dir::Left => {
+            if point.x == 0 {
+                return None;
+            }
+            return Some((point.x - 1, point.y));
+        }
+        Dir::Right => {
+            if point.x == map.x - 1 {
+                return None;
+            }
+            return Some((point.x + 1, point.y));
+        }
+        Dir::Up => {
+            if point.y == 0 {
+                return None;
+            }
+            return Some((point.x, point.y - 1));
+        }
+        Dir::Down => {
+            if point.y == map.y - 1 {
+                return None;
+            }
+            return Some((point.x, point.y + 1));
+        }
+    }
 }
--- src/day12/point.rs
+++ src/day12/point.rs
@@ -1,3 +1,42 @@
+use std::cell::{Ref, RefCell, RefMut};
+
+#[derive(Debug)]
+pub struct Map {
+    pub x: usize,
+    pub y: usize,
+    points: Vec<Vec<RefCell<Point>>>,
+}
+
+impl Map {
+    pub fn new(x: usize, y: usize) -> Self {
+        Map {
+            points: Vec::new(),
+            x,
+            y,
+        }
+    }
+    pub fn add_point(&mut self, point: Point) {
+        let y = point.y;
+        let mut line = self.points.get_mut(y);
+        if (line.is_none()) {
+            self.points.push(Vec::new());
+            line = self.points.get_mut(y);
+        }
+        line.unwrap().push(RefCell::new(point));
+    }
+    pub fn get_point(&self, x: usize, y: usize) -> Option<&RefCell<Point>> {
+        let line = match self.points.get(y) {
+            None => return None,
+            Some(line) => line,
+        };
+
+        match line.get(x) {
+            None => None,
+            Some(p) => Some(p),
+        }
+    }
+}
+
 #[derive(Debug)]
 pub struct Point {
     pub x: usize,
@@ -6,7 +45,7 @@ pub struct Point {
     pub letter: String,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub enum Dir {
     Up,
     Down,
@@ -15,23 +54,30 @@ pub enum Dir {
 }
 
 impl Point {
-    pub fn init() -> Self {
+    pub fn init(x: usize, y: usize, letter: String) -> Self {
         Point {
-            x: 0,
-            y: 0,
-            letter: String::from(""),
+            x,
+            y,
+            letter,
             move_dir: Vec::new(),
         }
     }
-    pub fn update_pos(&mut self, x: usize, y: usize) {
-        self.x = x;
-        self.y = y;
+    pub fn has_letter(&self, letter: &str) -> bool {
+        self.letter == String::from(letter)
     }
-    pub fn set_letter(&mut self, letter: String) {
-        self.letter = letter;
-    }
+
     pub fn set_move_dir(&mut self, dir: Vec<Dir>) {
         self.move_dir = dir;
     }
-    pub fn can_move(&mut self, dir: &Point) {}
+    pub fn can_move(&self, dir: Ref<Point>) -> bool {
+        if self.letter == String::from("S") {
+            return true;
+        }
+        let self_char = self.letter.chars().nth(0).unwrap() as u8;
+        let dir_char = dir.letter.chars().nth(0).unwrap() as u8;
+        if dir_char - self_char <= 1 {
+            return true;
+        }
+        return false;
+    }
 }
