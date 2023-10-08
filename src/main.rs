2b70eb19815b94bd37141fd8efafed020be213c3
张世阳 <zsytssk@gmail.com>
2023-10-08 18:21:14 +0800

    [master]

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ mod day1;
 mod day10;
 mod day11;
 mod day12;
+mod day13;
 mod day2;
 mod day3;
 mod day4;
@@ -24,5 +25,6 @@ fn main() {
     // day9::parse();
     // day10::parse();
     // day11::parse();
-    day12::parse();
+    // day12::parse();
+    day13::parse();
 }
