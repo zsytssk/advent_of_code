b6c2e78e2fcb1adf3ec83b32633f6eee45203c1f
张世阳 <zsytssk@gmail.com>
2023-10-09 10:10:56 +0800

    [master]

--- src/day13/list.rs
+++ src/day13/list.rs
@@ -1,3 +1,33 @@
+#[derive(Debug)]
+pub enum Token {
+    Num(i32),
+    Dot,
+    EndOfArr,
+    StartOfArr,
+}
+
+impl Token {
+    pub fn new(s: char) -> Self {
+        match s {
+            '[' => Token::StartOfArr,
+            ']' => Token::EndOfArr,
+            ',' => Token::Dot,
+            x => Token::Num(x.to_digit(10).unwrap() as i32),
+        }
+    }
+    pub fn append_num(&mut self, num: &str) {
+        match self {
+            Token::Num(x) => {
+                *x = format!("{}{}", x, num).parse::<i32>().unwrap()
+            }
+            Token::Dot => {}
+            Token::EndOfArr => {}
+            Token::StartOfArr => {}
+        }
+    }
+}
+
+#[derive(Debug)]
 pub enum ListItem {
     Num(i32),
     List(Vec<ListItem>),
--- src/day13/mod.rs
+++ src/day13/mod.rs
@@ -11,6 +11,7 @@ pub fn parse() {
 
 fn parse1() {
     let content = parse_input();
+    println!("{:?}", content);
 }
 
 fn parse_input() -> Vec<(ListItem, ListItem)> {
@@ -29,15 +30,78 @@ fn parse_input() -> Vec<(ListItem, ListItem)> {
 
 pub fn parse_line(str: &str) -> ListItem {
     let items = str.split("").filter(|&c| c != "").collect::<Vec<_>>();
+    let mut local_str = str.clone();
+    let mut tokens = Vec::new();
+    loop {
+        if local_str.len() == 0 {
+            break;
+        }
+        let (len, token) = peek(&local_str);
+        local_str = &local_str[len..local_str.len()];
+        tokens.push(token);
+    }
+    let (_, arr) = parse_tokens(0, &tokens);
 
-    for item in items {
-        match item {
-            "[" => {}
-            "]" => {}
-            "," => {}
-            x => {}
+    arr
+}
+
+pub fn parse_tokens(
+    start_index: usize,
+    tokens: &Vec<Token>,
+) -> (usize, ListItem) {
+    let mut vec: Vec<ListItem> = Vec::new();
+
+    let mut index = start_index;
+    loop {
+        let token_wrap = tokens.get(index);
+        if (token_wrap.is_none()) {
+            break;
+        }
+        let token = token_wrap.unwrap();
+        match token {
+            Token::StartOfArr => {
+                let (next_index, list) = parse_tokens(index + 1, tokens);
+                vec.push(list);
+                index = next_index;
+            }
+            Token::EndOfArr => {
+                break;
+            }
+            Token::Num(x) => vec.push(ListItem::Num(*x)),
+            Token::Dot => {}
+        }
+
+        index += 1;
+    }
+
+    (index, ListItem::List(vec))
+}
+
+pub fn peek(str: &str) -> (usize, Token) {
+    let mut s = str.chars().collect::<Vec<_>>();
+    let mut num = String::new();
+    let mut is_num = false;
+
+    let first = s.get(0).unwrap();
+
+    for c in str.chars() {
+        if is_num == false {
+            let token = Token::new(c);
+            match token {
+                Token::Num(x) => {
+                    is_num = true;
+                    num = format!("{}", x)
+                }
+                _ => return (1, token),
+            }
+            continue;
+        }
+        let token = Token::new(c);
+        match token {
+            Token::Num(n) => num = format!("{}{}", n, num),
+            _ => break,
         }
     }
-    println!("{:?}", str);
-    ListItem::Num(0)
+
+    (num.len(), Token::Num(num.parse::<i32>().unwrap()))
 }
