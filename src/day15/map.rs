d20f7f1629a5ef2d90d2fe02756e2dec6ad178eb
张世阳 <zsytssk@gmail.com>
2023-10-12 19:45:08 +0800

    test content [master]

--- src/day15/map.rs
+++ src/day15/map.rs
@@ -4,7 +4,6 @@ use std::{
 };
 
 pub struct Map {
-    pub points: Vec<Vec<RefCell<Point>>>,
     pub range_x: (i32, i32),
     pub range_y: (i32, i32),
     pub sensor_and_bean: Vec<((i32, i32), (i32, i32))>,
@@ -12,17 +11,7 @@ pub struct Map {
 
 impl Map {
     pub fn new(range: ((i32, i32), (i32, i32))) -> Self {
-        let mut points = Vec::new();
-        for y in range.1 .0..=range.1 .1 {
-            let mut line = Vec::new();
-            for x in range.0 .0..=range.0 .1 {
-                line.push(RefCell::new(Point::new(x, y)));
-            }
-            points.push(line);
-        }
-
         Map {
-            points,
             range_x: range.0,
             range_y: range.1,
             sensor_and_bean: Vec::new(),
@@ -31,54 +20,12 @@ impl Map {
     pub fn get_line_y(line_index: i32) -> i32 {
         todo!()
     }
-    pub fn set_sensor_and_bean(&mut self, info: Vec<(Point, Point)>) {
-        for item in info.iter() {
-            self.set_point((item.0.x, item.0.y), PointStatus::Sensor);
-            self.set_point((item.1.x, item.1.y), PointStatus::Beacon);
-        }
 
+    pub fn set_sensor_and_bean(&mut self, info: Vec<(Point, Point)>) {
         self.sensor_and_bean = info
             .iter()
             .map(|item| ((item.0.x, item.0.y), (item.1.x, item.1.y)))
             .collect::<Vec<_>>();
-    }
-    pub fn set_point(&self, pos: (i32, i32), status: PointStatus) {
-        if (pos.0 < self.range_x.0 || pos.0 > self.range_x.1)
-            || (pos.1 < self.range_y.0 || pos.1 > self.range_y.1)
-        {
-            return;
-        }
-
-        let x = (pos.0 - self.range_x.0) as usize;
-        let y = (pos.1 - self.range_y.0) as usize;
-
-        self.points[y][x].borrow_mut().set_status(status);
-    }
-    pub fn get_point(&self, pos: (i32, i32)) -> Option<RefMut<Point>> {
-        if (pos.0 < self.range_x.0 || pos.0 > self.range_x.1)
-            || (pos.1 < self.range_y.0 || pos.1 > self.range_y.1)
-        {
-            return None;
-        }
-
-        let x = (pos.0 - self.range_x.0) as usize;
-        let y = (pos.1 - self.range_y.0) as usize;
-
-        Some(self.points[y][x].borrow_mut())
-    }
-}
-
-impl fmt::Debug for Map {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let mut list = String::from("");
-        for line in self.points.iter() {
-            for point in line.iter() {
-                list.push_str(&point.borrow().to_map_string());
-            }
-            list.push_str("\n");
-        }
-
-        write!(f, "{}", list)
     }
 }
 
@@ -114,27 +61,7 @@ impl Point {
             status: PointStatus::Empty,
         }
     }
-    pub fn set_status(&mut self, status: PointStatus) {
-        self.status = status;
-    }
-    pub fn to_map_string(&self) -> String {
-        let s = match self.status {
-            PointStatus::Empty => ".",
-            PointStatus::ConfirmEmpty => "#",
-            PointStatus::Sensor => "S",
-            PointStatus::Beacon => "B",
-        };
-        String::from(s)
-    }
-    pub fn is_not_empty(&self) -> bool {
-        match self.status {
-            PointStatus::Empty => false,
-            PointStatus::ConfirmEmpty => false,
-            PointStatus::Sensor => true,
-            PointStatus::Beacon => true,
-        }
-    }
-    pub fn distance_from_point(&self, point: RefMut<Point>) -> i32 {
+    pub fn distance_from_point(&self, point: &Point) -> i32 {
         (self.x - point.x).abs() + (self.y - point.y).abs()
     }
     pub fn distance_from_line(&self, line_index: i32) -> i32 {
--- src/day15/mod.rs
+++ src/day15/mod.rs
@@ -1,5 +1,9 @@
 #![allow(unused)]
-use std::{borrow::BorrowMut, collections::HashMap, hash::Hash};
+use std::{
+    borrow::BorrowMut,
+    collections::{HashMap, HashSet},
+    hash::Hash,
+};
 
 use crate::utils::read_file;
 mod map;
@@ -7,63 +11,85 @@ mod map;
 use map::*;
 
 pub fn parse() {
-    parse1();
-    // parse2();
+    // parse1();
+    parse2();
 }
 
 fn parse1() {
     let mut map = parse_input();
-    println!("{:?}", map);
-    return;
+
     let line_index: i32 = 10;
-    let mut line_empty_list = Vec::new();
+    let mut range_list = Vec::new();
+    let has_num = 0;
     for item in map.sensor_and_bean.iter() {
-        let sensor_wrap = map.get_point(item.0);
-        let bean_wrap = map.get_point(item.1);
-        if sensor_wrap.is_none() || bean_wrap.is_none() {
-            panic!("not find points for sensor={:?} bean={:?}", item.0, item.1);
-        }
-        let (sensor, bean) = (sensor_wrap.unwrap(), bean_wrap.unwrap());
-        let sensor_distance = sensor.distance_from_point(bean);
+        let sensor = Point::new(item.0 .0, item.0 .1);
+        let bean = Point::new(item.1 .0, item.1 .1);
+        let sensor_distance = sensor.distance_from_point(&bean);
         let line_dis = sensor.distance_from_line(line_index);
         let distance_extra = sensor_distance - line_dis;
+
         if distance_extra < 0 {
             continue;
         }
 
         let empty_range_x =
             (sensor.x - distance_extra, sensor.x + distance_extra);
-        for i in empty_range_x.0..=empty_range_x.1 {
-            if sensor.y == line_index && i == sensor.x {
+        range_list.push((sensor.x - distance_extra, sensor.x + distance_extra))
+    }
+
+    let mut uniques = HashSet::new();
+    let mut remove_list = map
+        .sensor_and_bean
+        .iter()
+        .map(|item| vec![item.0, item.1])
+        .collect::<Vec<_>>()
+        .concat()
+        .iter()
+        .map(|item| (item.0, item.1))
+        .filter(|item| uniques.insert(format!("{},{}", item.0, item.1)))
+        .filter(|item| item.1 == line_index)
+        .collect::<Vec<_>>();
+
+    println!("remove_num={:?}\nrange_list:{:?}", remove_list, range_list);
+    let new_list = merge_ranges(range_list);
+    let num = get_ranges_num(&new_list) - remove_list.len() as i32;
+    println!("num={:?}\nnew_list:{:?}", num, new_list);
+}
+
+fn parse2() {
+    let mut map = parse_input();
+    let mix_x = 0;
+    let max_x = 4000000;
+    for line_index in 0..=4000000 {
+        let mut range_list = Vec::new();
+        let has_num = 0;
+        for item in map.sensor_and_bean.iter() {
+            let sensor = Point::new(item.0 .0, item.0 .1);
+            let bean = Point::new(item.1 .0, item.1 .1);
+            let sensor_distance = sensor.distance_from_point(&bean);
+            let line_dis = sensor.distance_from_line(line_index);
+            let distance_extra = sensor_distance - line_dis;
+
+            if distance_extra < 0 {
                 continue;
             }
-            if i == 2 {
-                println!(
-                    "sensor={:?} sensor_distance={:?}",
-                    sensor, sensor_distance
-                );
-            }
-            if line_empty_list.contains(&i) == false {
-                line_empty_list.push(i);
-            }
+
+            let empty_range_x =
+                (sensor.x - distance_extra, sensor.x + distance_extra);
+            range_list
+                .push((sensor.x - distance_extra, sensor.x + distance_extra))
         }
+
+        let new_list = merge_ranges(range_list);
+        if new_list.len() == 0
+            || (new_list.len() == 1
+                && new_list[0].0 <= mix_x
+                && new_list[0].1 >= max_x)
+        {
+            continue;
+        }
+        println!("line_index={:?} new_list:{:?}", line_index, new_list);
     }
-    line_empty_list.sort();
-    line_empty_list.retain(|x: &i32| {
-        let mut point = map.get_point((*x, line_index)).unwrap();
-        if point.is_not_empty() {
-            return false;
-        }
-        point.set_status(PointStatus::ConfirmEmpty);
-        return true;
-    });
-    for (index, x) in line_empty_list.iter().enumerate() {}
-    println!("{:?}", map);
-    println!(
-        "num={:?}\nline={:?}",
-        line_empty_list.len(),
-        line_empty_list,
-    );
 }
 
 fn parse_input() -> Map {
@@ -118,4 +144,36 @@ fn get_map_range(info: &Vec<(Point, Point)>) -> ((i32, i32), (i32, i32)) {
     }
 
     return ((min_x, max_x), (min_y, max_y));
+}
+
+fn merge_ranges(mut ranges: Vec<(i32, i32)>) -> Vec<(i32, i32)> {
+    ranges.sort_by(|a, b| a.0.cmp(&b.0));
+
+    let mut res: Vec<(i32, i32)> = Vec::new();
+    for item in ranges.iter() {
+        let len = res.len();
+        if len == 0 {
+            res.push((item.0, item.1));
+            continue;
+        }
+        let res_last_item = res.get_mut(len - 1).unwrap();
+        if item.0 > res_last_item.1 + 1 {
+            res.push((item.0, item.1));
+        } else {
+            if (item.1 > res_last_item.1) {
+                res_last_item.1 = item.1;
+            }
+        }
+    }
+
+    res
+}
+
+fn get_ranges_num(ranges: &Vec<(i32, i32)>) -> i32 {
+    let mut num = 0;
+    for item in ranges.iter() {
+        num += item.1 - item.0 + 1;
+    }
+
+    num
 }
