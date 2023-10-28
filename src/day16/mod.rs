1aa9c786afdc7219dfbfcc1fadc4c36c1f528dd9
shiyangzhang <shiyangzhang@whale.im>
2023-10-28 11:57:35 +0800

    [dev2]

--- src/day16/mod.rs
+++ src/day16/mod.rs
@@ -59,7 +59,7 @@ fn calc_top_path(
     complete_path_map: &mut PathMap,
     short_path: &HashMap<(String, String), usize>,
     map: &Switches,
-) -> Vec<(Vec<(String, bool)>)> {
+) -> Vec<(MapKey)> {
     let mut path_arr: Vec<_> = complete_path_map.iter().collect();
     path_arr.sort_by(|a, b| b.1.cmp(&a.1));
     let big_num = path_arr[0].1;
@@ -77,11 +77,32 @@ fn calc_top_path(
         if big_score + *score < *big_num {
             return false;
         }
-        arr.push();
+        arr.push((key.clone(), score.clone()));
         return true;
     });
 
-    loop_path_map.
+    if arr.len() == 0 {
+        return vec![];
+    }
+
+    arr.sort_by(|a, b| {
+        let rate_cmp = b.1.cmp(&a.1);
+        if rate_cmp != std::cmp::Ordering::Equal {
+            return rate_cmp;
+        }
+        b.0.get_time().cmp(&a.0.get_time())
+        // b.0.sum_time().cmp(&a.0.sum_time())
+        // rate_cmp
+    });
+
+    let big_score = arr[0].1;
+    let big_time = arr[0].0.get_time();
+
+    arr.into_iter()
+        .filter(|item| item.1 == big_score && item.0.get_time() == big_time)
+        .map(|item| item.0)
+        // .take(200)
+        .collect::<Vec<_>>()
 }
 
 fn parse_input() -> Switches {
