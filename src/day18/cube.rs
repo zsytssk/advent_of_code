7a6f3b78edda29bde3856834698620e10bbccd9e
shiyangzhang <shiyangzhang@whale.im>
2023-10-31 15:22:06 +0800

    [dev2]

---
+++ src/day18/cube.rs
@@ -0,0 +1,138 @@
+pub struct CubeList {
+    cubes: Vec<Cube>,
+}
+
+impl CubeList {
+    pub fn new(cubes: Vec<Cube>) -> Self {
+        CubeList { cubes }
+    }
+}
+
+#[derive(Debug)]
+pub struct Cube {
+    pub points: Vec<Point>,
+    pub surfaces: Vec<Surface>,
+    pub x: usize,
+    pub y: usize,
+    pub z: usize,
+}
+
+impl Cube {
+    pub fn new(x: usize, y: usize, z: usize) -> Self {
+        let mut points = vec![];
+        for x in x..=x + 1 {
+            for y in y..=y + 1 {
+                for z in z..=z + 1 {
+                    let point = Point::new(x, y, z);
+                    points.push(point);
+                }
+            }
+        }
+        let mut cube = Cube {
+            points,
+            x,
+            y,
+            z,
+            surfaces: vec![],
+        };
+
+        cube.init_surfaces();
+
+        cube
+    }
+    pub fn is_overlap(&self, other: &Cube) -> bool {
+        let mut overlap_points_num = 0;
+
+        for point in self.points.iter() {
+            if other.contain_point(point) {
+                overlap_points_num += 1
+            }
+        }
+
+        overlap_points_num == 4
+    }
+    pub fn contain_point(&self, point: &Point) -> bool {
+        self.points.contains(point)
+    }
+    pub fn init_surfaces(&mut self) {
+        let points = &self.points;
+        let x = self.x.clone();
+        let y = self.y.clone();
+        let z = self.z.clone();
+
+        let mut points1 = vec![];
+        let mut points2 = vec![];
+        for x in x..=x + 1 {
+            for y in y..=y + 1 {
+                let point1 = Point::new(x, y, z);
+                points1.push(point1);
+
+                let point2 = Point::new(x, y, z + 1);
+                points2.push(point2);
+            }
+        }
+        let surface1 = Surface::new(points1);
+        let surface2 = Surface::new(points2);
+
+        let mut points3 = vec![];
+        let mut points4 = vec![];
+        for x in x..=x + 1 {
+            for z in z..=z + 1 {
+                let point3 = Point::new(x, y, z);
+                points3.push(point3);
+
+                let point4 = Point::new(x, y + 1, z);
+                points4.push(point4);
+            }
+        }
+
+        let surface3 = Surface::new(points3);
+        let surface4 = Surface::new(points4);
+
+        let mut points5 = vec![];
+        let mut points6 = vec![];
+        for y in y..=y + 1 {
+            for z in z..=z + 1 {
+                let point5 = Point::new(x, y, z);
+                points5.push(point5);
+
+                let point6 = Point::new(x + 1, y, z);
+                points6.push(point6);
+            }
+        }
+
+        let surface5 = Surface::new(points5);
+        let surface6 = Surface::new(points6);
+
+        self.surfaces.push(surface1);
+        self.surfaces.push(surface2);
+        self.surfaces.push(surface3);
+        self.surfaces.push(surface4);
+        self.surfaces.push(surface5);
+        self.surfaces.push(surface6);
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub struct Surface {
+    points: Vec<Point>,
+}
+
+impl Surface {
+    pub fn new(points: Vec<Point>) -> Self {
+        Surface { points }
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub struct Point {
+    pub x: usize,
+    pub y: usize,
+    pub z: usize,
+}
+
+impl Point {
+    pub fn new(x: usize, y: usize, z: usize) -> Self {
+        Point { x, y, z }
+    }
+}
--- src/day18/mod.rs
+++ src/day18/mod.rs
@@ -1,17 +1,62 @@
 #![allow(unused)]
+use std::time::Instant;
+
 use crate::utils::read_file;
 
+use self::cube::Cube;
+
+mod cube;
+
 pub fn parse() {
-    parse1();
-    // parse2();
+    // parse1();
+    parse2();
 }
 
 fn parse1() {
-    let content = parse_input();
+    let now = Instant::now();
+    let list = parse_input();
+    let mut overlap_num = 0;
+    for i in 0..(list.len() - 1) {
+        for j in i + 1..list.len() {
+            let cube1 = &list[i];
+            let cube2 = &list[j];
+            if cube1.is_overlap(cube2) {
+                overlap_num += 1;
+            }
+        }
+    }
+    let res = 6 * list.len() - 2 * overlap_num;
+    println!("time={:?}|res={:?}", now.elapsed(), res);
 }
 
-fn parse_input() -> String {
-    let content = read_file("day12/input.txt").unwrap();
+fn parse2() {
+    let now = Instant::now();
+    let list = parse_input();
+    let mut overlap_num = 0;
+    for i in 0..(list.len() - 1) {
+        for j in i + 1..list.len() {
+            let cube1 = &list[i];
+            let cube2 = &list[j];
+            if cube1.is_overlap(cube2) {
+                overlap_num += 1;
+            }
+        }
+    }
+    let res = 6 * list.len() - 2 * overlap_num;
+    println!("time={:?}|res={:?}", now.elapsed(), res);
+}
+
+fn parse_input() -> Vec<Cube> {
+    let content = read_file("day18/demo.txt").unwrap();
 
     content
+        .split("\n")
+        .map(|item| {
+            let cor = item
+                .split(",")
+                .map(|x| x.parse::<usize>().unwrap())
+                .collect::<Vec<_>>();
+            Cube::new(cor[0], cor[1], cor[2])
+        })
+        .collect::<Vec<_>>()
 }
