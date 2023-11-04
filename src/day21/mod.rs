7c1fac2a8eac5e7ff71f96683b7d184407ec5dd2
shiyangzhang <shiyangzhang@whale.im>
2023-11-04 17:42:31 +0800

    [master]

--- src/day21/mod.rs
+++ src/day21/mod.rs
@@ -1,5 +1,10 @@
 #![allow(unused)]
+use std::{collections::HashMap, time::Instant};
+
 use crate::utils::read_file;
+
+mod operate;
+use operate::*;
 
 pub fn parse() {
   parse1();
@@ -7,11 +12,114 @@ pub fn parse() {
 }
 
 fn parse1() {
-  let content = parse_input();
+  let statements = parse_input();
+  let now = Instant::now();
+
+  let mut loop_arr: Vec<String> = vec![];
+  let mut save_arr: Vec<String> = vec![];
+  let mut res_map: HashMap<String, i64> = HashMap::new();
+
+  for item in statements.iter() {
+    match &item.op {
+      OperateWrap::Number(num) => {
+        res_map.insert(item.name.clone(), num.clone());
+      }
+      OperateWrap::Operate(op) => {
+        save_arr.push(item.name.clone());
+        continue;
+      }
+    }
+  }
+
+  println!(
+    "res_map_len={:?} | statements={:?}",
+    res_map.len(),
+    statements.len()
+  );
+
+  loop {
+    calc_top_list(&mut loop_arr, &mut save_arr, &res_map, &statements);
+
+    let mut remove_list = vec![];
+    let mut add_list = vec![];
+    for (index, item) in loop_arr.iter().enumerate() {
+      match res_map.get(item) {
+        Some(n) => {
+          remove_list.push(index);
+          continue;
+        }
+        None => {}
+      };
+
+      let s = statements.iter().find(|x| x.name == *item).unwrap();
+      let num = match &s.op {
+        OperateWrap::Number(n) => n.clone(),
+        OperateWrap::Operate(op) => {
+          let left = res_map.get(&op.left);
+          let right = res_map.get(&op.right);
+          if left.is_none() || right.is_none() {
+            if left.is_none() {
+              add_list.push(op.left.clone());
+            }
+            if right.is_none() {
+              add_list.push(op.right.clone());
+            }
+            continue;
+          }
+
+          match &op.opr {
+            Operator::Add => left.unwrap() + right.unwrap(),
+            Operator::Minus => left.unwrap() - right.unwrap(),
+            Operator::Multiply => left.unwrap() * right.unwrap(),
+            Operator::Divide => left.unwrap() / right.unwrap(),
+          }
+        }
+      };
+
+      res_map.insert(item.clone(), num);
+      remove_list.push(index);
+    }
+
+    loop_arr.clear();
+
+    if loop_arr.len() == 0 && save_arr.len() == 0 {
+      break;
+    }
+  }
+
+  println!(
+    "now={:?} | res={:?}",
+    now.elapsed(),
+    res_map.get("root").unwrap()
+  );
 }
 
-fn parse_input() -> String {
+fn calc_top_list(
+  loop_arr: &mut Vec<String>,
+  save_arr: &mut Vec<String>,
+  res_map: &HashMap<String, i64>,
+  statements: &Vec<Statement>,
+) {
+  save_arr.retain(|item| {
+    let statement = statements.iter().find(|x| x.name == *item).unwrap();
+    match &statement.op {
+      OperateWrap::Operate(op) => {
+        let left = res_map.get(&op.left);
+        let right = res_map.get(&op.right);
+        if left.is_none() || right.is_none() {
+          return true;
+        }
+
+        loop_arr.push(item.clone());
+        return false;
+      }
+      _ => panic!(),
+    };
+  });
+}
+
+fn parse_input() -> Vec<Statement> {
   let content = read_file("day21/input.txt").unwrap();
 
-  content
+  content.split("\n").map(Statement::form_str).collect()
 }
---
+++ src/day21/operate.rs
@@ -0,0 +1,68 @@
+#[derive(Debug)]
+pub enum Operator {
+  Add,
+  Minus,
+  Multiply,
+  Divide,
+}
+
+impl Operator {
+  pub fn form_str(s: &str) -> Self {
+    match s {
+      "+" => Operator::Add,
+      "-" => Operator::Minus,
+      "*" => Operator::Multiply,
+      "/" => Operator::Divide,
+      _ => unreachable!(),
+    }
+  }
+}
+
+#[derive(Debug)]
+pub struct Operate {
+  pub left: String,
+  pub opr: Operator,
+  pub right: String,
+}
+
+impl Operate {
+  pub fn form_str(s: &str) -> Self {
+    let mut split = s.split(" ").collect::<Vec<_>>();
+    let left = split[0].to_string();
+    let opr = Operator::form_str(split[1]);
+    let right = split[2].to_string();
+
+    Operate { left, opr, right }
+  }
+}
+
+#[derive(Debug)]
+pub enum OperateWrap {
+  Number(i64),
+  Operate(Operate),
+}
+
+impl OperateWrap {
+  pub fn form_str(s: &str) -> Self {
+    if let Ok(num) = s.parse::<i64>() {
+      OperateWrap::Number(num)
+    } else {
+      OperateWrap::Operate(Operate::form_str(s))
+    }
+  }
+}
+
+#[derive(Debug)]
+pub struct Statement {
+  pub name: String,
+  pub op: OperateWrap,
+}
+
+impl Statement {
+  pub fn form_str(s: &str) -> Self {
+    let mut split = s.split(": ").collect::<Vec<_>>();
+    let name = split[0].to_string();
+    let op = OperateWrap::form_str(split[1]);
+    Statement { name, op }
+  }
+}
